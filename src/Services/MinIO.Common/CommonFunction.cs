130024bbbc3243cf3731f4e6aac4988d08660f5c
Nguyen Thang <thangmt1997@gmail.com>
2019-11-11 22:29:01 +0700

    Tạo khung API

---
+++ src/Services/MinIO.Common/CommonFunction.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinIO.Common
+{
+    class CommonFunction
+    {
+    }
+}
---
+++ src/Services/MinIO.Services/FileUploadService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinIO.Services
+{
+    class FileUploadService
+    {
+    }
+}
